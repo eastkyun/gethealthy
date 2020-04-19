439f0f5a397dcf1c6e4e24f9c29a3616f72bf614
t-ahere <52394290+t-ahere@users.noreply.github.com>
2020-04-19 13:48:21 +0900

    tag whilelist

--- src/main/java/com/gethealthy/gethealthy/settings/SettingsController.java
+++ src/main/java/com/gethealthy/gethealthy/settings/SettingsController.java
@@ -1,5 +1,7 @@
 package com.gethealthy.gethealthy.settings;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.gethealthy.gethealthy.account.AccountService;
 import com.gethealthy.gethealthy.account.CurrentUser;
 import com.gethealthy.gethealthy.account.tag.TagRepository;
@@ -19,6 +21,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.validation.Valid;
+import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -51,6 +54,9 @@ public class SettingsController {
 
     @Autowired
     private TagRepository tagRepository;
+
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @InitBinder("passwordForm")
     public void passwordFormInitBinder(WebDataBinder webDataBinder){
@@ -121,10 +127,14 @@ public class SettingsController {
     }
 
     @GetMapping(SETTINGS_TAGS_URL)
-    public String updateTags(@CurrentUser Account account, Model model){
+    public String updateTags(@CurrentUser Account account, Model model) throws JsonProcessingException {
         model.addAttribute(account);
         Set<Tag> tags = accountService.getTags(account);
         model.addAttribute("tags",tags.stream().map(Tag::getTitle).collect(Collectors.toList()));
+
+        List<String> allTags = tagRepository.findAll().stream().map(Tag::getTitle).collect(Collectors.toList());
+        model.addAttribute("whitelist", objectMapper.writeValueAsString(allTags));
+
         return SETTINGS_TAGS_VIEW_NAME;
     }
 
