5f6372b489dff7755efb2195a2c2cebc104bc776
KwonSuBin <>
2020-05-11 17:08:34 +0900

    correct error in cart

--- src/main/resources/static/js/comma.js
+++ src/main/resources/static/js/comma.js
@@ -2,6 +2,6 @@ function numberWithCommas() {
     price = document.querySelectorAll(".price");
 
     price.forEach(function(p){
-        p.textContent = p.textContent.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") + '원';
+        p.textContent = p.textContent.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
     });
 }
